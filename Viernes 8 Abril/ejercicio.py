096582ab5c9115897670a31c3e1af90d15b3f32b
Alvaro Poveda <alvaropoveda@ingenieros.com>
2022-05-23 08:49:42 -0500

    first commit

---
+++ Viernes 8 Abril/ejercicio.py
@@ -0,0 +1,8 @@
+# Dada una lista por ejemplo [10, -14, 26, 5, -3, 13, -5]
+#  encontrar si al menos la suma de dos valores de esa lista da 0
+
+
+# Ejercicio 2
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+# Se espera -> ['My', 'name', 'is', 'Kelly']
